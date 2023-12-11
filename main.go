aaa3a7c4c7f490d97aef0a9407a071e3d245b518
ttwange <ttwange2@gmail.com>
2023-12-11 09:04:09 +0300

    writing first program in GO

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("hello world")
+	fmt.Println("Welcome to our GO conference booking application")
+	fmt.Println("Get your tickets here to attend")
 }

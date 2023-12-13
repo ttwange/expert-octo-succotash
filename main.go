61d44d1b5cb0857c9f2ef35ca72982fea96660b7
ttwange <ttwange2@gmail.com>
2023-12-13 19:52:14 +0300

    constants are like variables, except they dont change

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
+	var remainingTickets = 50
 
 	fmt.Println("Welcome to our GO conference booking application")
 	fmt.Println("Get your tickets here to attend")
+	fmt.Println("We have a total of", conferenceTickets, "and we have", remainingTickets, "remaining ")
 	
 	fmt.Println((conferenceName))
 }

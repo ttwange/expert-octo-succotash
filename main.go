c6c35d6297ca0645d3b2f42db1ae2d9065cc048c
ttwange <ttwange2@gmail.com>
2023-12-13 19:39:55 +0300

    Working on variables & constrains

--- main.go
+++ main.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to our GO conference booking application")
 	fmt.Println("Get your tickets here to attend")
+	
+	var conferenceName = "Go Conference"
+	fmt.Println((conferenceName))
 }
+
